4c1ece6957ee0fa35a3dffaf873839096d0f7251
mvanmoer <mvanmoer@illinois.edu>
2023-11-02 16:48:08 -0500

    Config for 3.2 compatible ILLIXR/InfiniTAM branch

--- cmake/GetInfiniTAM.cmake
+++ cmake/GetInfiniTAM.cmake
@@ -1,17 +1,11 @@
 # module to download, build and install the InfiniTAM ILLIXR plugin
 
-# get dependencies
-#get_external(PortAudio)
-#get_external(SpatialAudio)
-
 set(INFINITAM_CMAKE_ARGS "")
 
 ExternalProject_Add(InfiniTAM
         GIT_REPOSITORY https://github.com/ILLIXR/InfiniTAM.git        # Git repo for source code
-        #        GIT_TAG cb686800157b7bc68ab46f564abb4a0e24ffbeec              # via Yihan on slack
-        GIT_TAG 264de7919c0c2c06ee8b7d5284535d84a3f54c06 # MVM's first push
+        GIT_TAG 82b931520a9e29339e21a8ae16dcde5570a013f6              # 2023-11-02 commit that builds and installs
         PREFIX ${CMAKE_BINARY_DIR}/_deps/infinitam                    # the build directory
-        #DEPENDS ${PortAudio_DEP_STR} ${SpatialAudio_DEP_STR} ${OpenCV_DEP_STR}  # dependencies of this module
         #arguments to pass to CMake
         CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_CXX_FLAGS=-L${CMAKE_INSTALL_PREFIX}/lib\ -L${CMAKE_INSTALL_PREFIX}/lib64 -DILLIXR_ROOT=${PROJECT_SOURCE_DIR}/include -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX} -DILLIXR_BUILD_SUFFIX=${ILLIXR_BUILD_SUFFIX} ${INFINITAM_CMAKE_ARGS}
         )
