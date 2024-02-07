773cf98a7c1d78a779a12f42b6d8fdb2378e2970
astro-friedel <friedel@illinois.edu>
2024-02-07 15:17:00 -0600

    updated the docker build script

--- docker/build_docker.py
+++ docker/build_docker.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import os
-import docker
-from docker.errors import DockerException, ImageNotFound
+import sys
+from docker import APIClient
+from docker.errors import DockerException
 
 from images import docker_images, make_list
+
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
 
 
 def parse_args(ch):
@@ -20,29 +24,37 @@ def parse_args(ch):
 def build(images):
     # try to connect to Docker daemon
     try:
-        client = docker.DockerClient(base_url=f'unix://{os.getenv("HOME")}/.docker/desktop/docker.sock')
+        client = APIClient(base_url=f'unix://{os.getenv("HOME")}/.docker/desktop/docker.sock')
     except DockerException as _:
         try:
-            client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
+            client = APIClient(base_url='unix:///var/run/docker.sock')
         except DockerException as _:
             try:
-                client = docker.DockerClient(base_url='tcp://127.0.0.1:1234')
+                client = APIClient(base_url='tcp://127.0.0.1:1234')
             except DockerException as _:
                 raise RuntimeError("Could not connect to a Docker daemon. Please start Docker and try again.")
     print("Searching for ILLIXR Docker images...")
     for image in images:
         ops, ver = image.lower().split("-")
-        try:
-            img = client.images.get(f"illixr_{ops}:{ver}")
+        img = client.images(f"illixr_{ops}:{ver}")
+        if img:
+            img = img[0]
             print(f"Image illixr_{ops}:{ver} already exists. Removing previous version...")
-            images.remove(img)
-        except ImageNotFound as _:
-            pass
+            client.remove_image(img['Id'], force=True, norpune=True)
     print("Building Docker images...")
     for image in images:
         ops, ver = image.lower().split("-")
         print(f"Building illixr_{ops}:{ver}...")
-        client.images.build(path=f"{ops}/{ver}", tag=f"illixr_{ops}:{ver}")
+        for line in client.build(path=f"{script_directory}/{ops}/{ver}", tag=f"illixr_{ops}:{ver}"):
+            data = line.decode("utf-8")
+            if data:
+                data = json.loads(data.strip())
+                if "stream" in data:
+                    print(data["stream"])
+                elif "error" in data:
+                    print(f"Error: {data['error']}"
+                else:
+                    print("\n"))
     print("  Done")
 
 
